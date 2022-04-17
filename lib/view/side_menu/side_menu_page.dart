172a3710219d8a23ce45d8cf0aea9754591560d1
Hide Toyoda <hide.toyoda@gmail.com>
2022-04-17 22:51:27 +0900

    create side menu page

--- lib/view/side_menu/side_menu_page.dart
+++ lib/view/side_menu/side_menu_page.dart
@@ -2,13 +2,42 @@ import 'package:flutter/material.dart';
 
 import '../style.dart';
 
+import '../../util/constants.dart';
+
 class SideMenuPage extends StatelessWidget {
   const SideMenuPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: PageColor.sideMenuBgColor,
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Column(
+              children: [
+                FlutterLogo(
+                  size: 100.0,
+                ),
+                Text(StringR.appTitle),
+              ],
+            ),
+          ),
+          ListTile(
+            title: Text(StringR.addNewTask),
+            onTap: () => _addNewTask(context: context),
+          ),
+          SwitchListTile(
+            value: false,
+            onChanged: null,
+            title: Text(StringR.isFinishedTaskIncluded),
+          ),
+        ],
+      ),
     );
   }
+
+  ///
+  _addNewTask({required BuildContext context}) {}
 }
