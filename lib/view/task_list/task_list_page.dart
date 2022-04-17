5a8fac9a06cd1b0dd0d1dfa20cdab66cf6dd79d2
Hide Toyoda <hide.toyoda@gmail.com>
2022-04-17 22:21:45 +0900

    create floating button at task screen

--- lib/view/task_list/task_list_page.dart
+++ lib/view/task_list/task_list_page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../style.dart';
+
+import '../../util/constants.dart';
+import '../../view_model/view_model.dart';
 
 class TaskListPage extends StatelessWidget {
   const TaskListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: PageColor.taskListBgColor,
+    return Consumer<ViewModel>(
+      builder: (context, vm, child) {
+        final screenSize = vm.screenSize;
+
+        return Scaffold(
+          backgroundColor: PageColor.taskListBgColor,
+          appBar: AppBar(
+            title: Text(StringR.taskList),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                onPressed: () => _sort(context: context),
+                icon: const Icon(Icons.sort),
+              ),
+            ],
+          ),
+          floatingActionButton: (screenSize == ScreenSize.LARGE)
+              ? null
+              : FloatingActionButton(
+                  onPressed: () => _addNewTask(context: context),
+                  child: const Icon(Icons.add),
+                ),
+        );
+      },
     );
   }
+
+  ///
+  _sort({required BuildContext context}) {}
+
+  ///
+  _addNewTask({required BuildContext context}) {}
 }
